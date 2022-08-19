642fcc277395a471bb3015ab40c788885478ad3c
alissonfernandes <alissonfernandes200@gmail.com>
2022-08-19 20:55:36 -0300

    bean service configured in avaliacaoFisicaServiceImpl

--- src/main/java/br/com/alissonfernandes/academiabackend/service/impl/AvaliacaoFisicaServiceImpl.java
+++ src/main/java/br/com/alissonfernandes/academiabackend/service/impl/AvaliacaoFisicaServiceImpl.java
@@ -7,10 +7,12 @@ import br.com.alissonfernandes.academiabackend.model.AvaliacaoFisica;
 import br.com.alissonfernandes.academiabackend.repository.AvaliacaoFisicaRepository;
 import br.com.alissonfernandes.academiabackend.service.IAvaliacaoFisicaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class AvaliacaoFisicaServiceImpl implements IAvaliacaoFisicaService {
 
     @Autowired
