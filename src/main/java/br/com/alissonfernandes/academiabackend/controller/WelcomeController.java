4ac3abfed3a4f80ed5e04c74c56aabf22d1e3fa1
alissonfernandes <alissonfernandes200@gmail.com>
2022-08-19 22:33:53 -0300

    configuring adapter

--- src/main/java/br/com/alissonfernandes/academiabackend/controller/WelcomeController.java
+++ src/main/java/br/com/alissonfernandes/academiabackend/controller/WelcomeController.java
@@ -1,6 +1,5 @@
 package br.com.alissonfernandes.academiabackend.controller;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +12,11 @@ public class WelcomeController {
     }
 
     @GetMapping("/users")
-    @PreAuthorize("hasAnyRole('MANAGERS', 'USERS')")
     public String users() {
         return "Authorized user";
     }
 
     @GetMapping("/managers")
-    @PreAuthorize("hasAnyRole('MANAGERS')")
     public String managers() {
         return "Authorized managers";
     }
--- src/main/java/br/com/alissonfernandes/academiabackend/security/WebSecurityConfig.java
+++ src/main/java/br/com/alissonfernandes/academiabackend/security/WebSecurityConfig.java
@@ -1,8 +1,10 @@
 package br.com.alissonfernandes.academiabackend.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -22,5 +24,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("{noop}admin123")
                 .roles("MANAGERS");
         ;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/").permitAll()
+                .antMatchers("login").permitAll()
+                .antMatchers("/managers").hasAnyRole("MANAGERS")
+                .antMatchers("/users").hasAnyRole("USERS", "MANAGERS")
+                .anyRequest().authenticated().and().formLogin();
     }
 }
